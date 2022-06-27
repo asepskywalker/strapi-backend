5fe6fcc464b3eef78454e3eee2f94245880f2224
Rahmat Subandi <rahmat@indonesiacrypto.network>
2022-06-27 16:01:23 +0700

    clean cicd

--- config/database.js
+++ config/database.js
@@ -7,7 +7,7 @@ module.exports = ({ env }) => ({
         client: "mysql",
         host: env("DATABASE_HOST", "127.0.0.1"),
         port: env.int("DATABASE_PORT", 3306),
-        database: env("DATABASE_NAME", "blog"),
+        database: env("DATABASE_NAME", "sample"),
         username: env("DATABASE_USERNAME", "root"),
         password: env("DATABASE_PASSWORD", ""),
         ssl: env.bool("DATABASE_SSL", false),
